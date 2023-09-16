d5bcd450d709b3ce39619e62c6f226ceece8fbf3
Ismael <lindoespinoso@gmail.com>
2023-09-16 04:19:39 +0200

    first commit

---
+++ enviar_correo.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    $to = "erab30@yahoo.es"; // Cambia esta dirección de correo electrónico por la tuya
+    $subject = "Nuevo mensaje de contacto de $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    mail($to, $subject, $message, $headers);
+}
+?>
