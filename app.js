275679eebbeaf0f24c846a9a185c4cc0f4bf5939
Ismael <lindoespinoso@gmail.com>
2023-09-17 00:01:50 +0200

    v0.0.1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const enviarCorreo = require('./enviar_correo');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Ruta para servir archivos HTML y procesar el formulario
+app.use(express.static('public'));
+
+// Ruta para procesar el formulario
+app.post('/enviar-formulario', (req, res) => {
+    const { name, email, message } = req.body;
+
+    // Llama a la función enviarCorreo para enviar el correo
+    enviarCorreo(name, email, message);
+
+    // Redirige a una página de agradecimiento
+    res.redirect('/gracias.html'); // Crea una página de agradecimiento (gracias.html)
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
---
+++ enviar_correo.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: `${process.env.SENDER}`,
+        pass: `${process.env.PASSWORD}`
+    }
+});
+
+
+function enviarCorreo(nombre, email, mensaje) {
+    const mailOptions = {
+        from: `${process.env.SENDER}`,
+        to: `${process.env.RECIVER}`,
+        subject: 'Nuevo mensaje de contacto de ' + nombre,
+        text: `Nombre: ${nombre}\nCorreo Electrónico: ${email}\nMensaje: ${mensaje}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo:', error);
+        } else {
+            console.log('Correo enviado:', info.response);
+        }
+    });
+}
+
+module.exports = enviarCorreo;
