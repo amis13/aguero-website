fa25c1a15ba60ad13fad87b5ff2fe630fd0c7f40
Ismael <lindoespinoso@gmail.com>
2023-09-16 23:37:04 +0200

    v0.0.1

--- enviar_correo.php
+++ enviar_correo.php
@@ -4,11 +4,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
     
-    $to = "erab30@yahoo.es";
+    $to = "erab30@yahoo.es"; // Cambia esta dirección de correo electrónico por la tuya
     $subject = "Nuevo mensaje de contacto de $name";
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     
     mail($to, $subject, $message, $headers);
+    
+    // Redireccionar después de enviar el correo
+    header("Location: gracias.html"); // Crea una página de agradecimiento (gracias.html)
+    exit;
 }
 ?>
