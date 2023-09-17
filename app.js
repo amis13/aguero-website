e3325e0a5366c4fad2ce2672bd9c33bf19de08fd
Ismael <lindoespinoso@gmail.com>
2023-09-17 02:03:59 +0200

    v0.0.1

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const enviarCorreo = require('./enviar_correo'); // Asegúrate de ajustar la ruta al archivo enviar_correo.js
+const enviarCorreo = require('./enviar_correo');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Ruta para servir archivos HTML desde la carpeta 'public'
 app.use(express.static('public'));
 
-// Ruta para procesar el formulario
 app.post('/enviar-formulario', (req, res) => {
     const { name, email, message } = req.body;
 
-    // Llama a la función enviarCorreo para enviar el correo
     enviarCorreo(name, email, message);
 
-    // Redirige a una página de agradecimiento
-    res.redirect('/gracias.html'); // Asegúrate de que gracias.html esté en la carpeta 'public'
+    res.redirect('/gracias.html');
 });
 
 const PORT = process.env.PORT || 3000;
